0d48d1f2257987390cf20725eca888a86545a85f
imruahmed <im.ahmed97@gmail.com>
2016-07-09 20:42:49 -0700

    Add MapsActivity

--- app/src/main/java/io/github/imruahmed/opti/MainActivity.java
+++ app/src/main/java/io/github/imruahmed/opti/MainActivity.java
@@ -1,13 +1,28 @@
 package io.github.imruahmed.opti;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mAddTripButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddTripButton = (Button) findViewById(R.id.add_trip_button);
+        mAddTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
